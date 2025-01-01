4f87a61cb5236d3f7f7d2623411a93e31b8bf92a
Justin Green <justingreendev@gmail.com>
2025-01-01 11:22:49 -0600

    Input example

--- Variables.c
+++ Variables.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  int age = 36;
-  printf("%d\n", age);
+  int num = 11;
+  float decimal = 1.2;
+  double precise = 1.212;
+  // char letter = 'J';
+  // char str[] = "Hello";
+  // printf("%d\n", num);
+  // printf("%d\n", letter)
+  printf("The number is %i\n", num);
+  printf("%f\n", decimal);
+  // printf("%s\n", str[]);
 }
