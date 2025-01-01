05e4b92c2bddf08e3ced1e15f510a739030f1798
Justin Green <justingreendev@gmail.com>
2025-01-01 12:02:41 -0600

    loops

---
+++ Loops.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  // for (int i = 1; i <= 5; i++) {
+  //   printf("Iteration %i\n", i);
+  // }
+
+  int count = 1;
+  while (count <= 7)
+  {
+    printf("Iteration %i\n", count);
+    count++;
+  }
+
+  // Infinite loop example
+  // while(1) {
+  //   printf("Hello World\n");
+  // }
+}
