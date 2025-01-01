f9c9972368c741129bb155518ecc43efa47161c4
Justin Green <justingreendev@gmail.com>
2025-01-01 11:55:29 -0600

    more conditionals

--- Conditionals.c
+++ Conditionals.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 int main(void) {
-  int age;
+  // int age;
 
-  printf("Please enter your age: ");
-  scanf("%i", &age);
+  // printf("Please enter your age: ");
+  // scanf("%i", &age);
 
-  if(age < 18) {
-    printf("You need to be 18 or older to continue\n");
-  }else {
-    printf("You may continue\n");
-  }
+  // if(age < 18) {
+  //   printf("You need to be 18 or older to continue\n");
+  // }else {
+  //   printf("You may continue\n");
+  // }
+
+  int x = 10;
+  int y = (x > 5) ? 100: 200;
+  printf("y: %i\n", y);
 }
