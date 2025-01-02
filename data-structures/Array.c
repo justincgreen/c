7f80373d3dad70aa162f0b18b30272f9ded2a35f
Justin Green <justingreendev@gmail.com>
2025-01-02 14:23:26 -0600

    LinkedList

--- data-structures/Array.c
+++ data-structures/Array.c
@@ -5,9 +5,17 @@ int main(void) {
   int nums_size = sizeof(nums);
   int nums_length = nums_size / sizeof(nums[0]);
 
-  printf("The array length is %i\n", nums_length);
+  // printf("The array length is %i\n", nums_length);
 
-  for(int i = 0; i < 3; i++) {
-    printf("Element value is %i\n", nums[i]);
-  }  
+  // for(int i = 0; i < 3; i++) {
+  //   printf("Element value is %i memory address is %p\n", nums[i], (void*)&nums[i]);
+  // }  
+
+  int numbers[3] = {10,11,12};
+  int numbers_size = sizeof(numbers); // 12 bytes =  3 numbers * 4 bytes
+  int numbers_length = numbers_size / sizeof(numbers[0]); 
+  
+  for(int i = 0; i < numbers_length; i++) {
+    printf("%i\n", numbers[i]);
+  }
 }
---
+++ data-structures/LinkedList.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Define the structure for a linked list node
+struct Node {
+  int data; // data part of the node
+  struct Node* next; // Pointer to the next node
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+  struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  return newNode;
+}
+
+// function to print the linked list
+void printList(struct Node* head) {
+  struct Node* temp = head;
+  while (temp != NULL) {
+    printf("%d ->", temp->data);
+    temp = temp->next;
+  }
+  printf("NULL\n");
+}
+
+int main(void) {
+  // Create the first node
+  struct Node* head = createNode(10);
+
+  // Add more nodes
+  head->next = createNode(20);
+  head->next->next = createNode(30);
+
+  // print the linked list
+  printf("Linked list: ");
+  printList(head);
+
+  // free allocated memory
+  struct Node* temp;
+  while (head != NULL) {
+    temp = head;
+    head = head->next;
+    free(temp);
+  }
+}
