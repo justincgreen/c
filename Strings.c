32d1bd505b5e6fd1d6c17e46ff68a066b12c9355
Justin Green <justingreendev@gmail.com>
2025-01-01 18:16:07 -0600

    concat

--- Strings.c
+++ Strings.c
@@ -7,10 +7,15 @@ int main(void) {
   // printf("%s\n", word);
 
   int length = strlen(word);  
-  printf("String length is %i\n", length);
+  // printf("String length is %i\n", length);
 
   char original[] = "Hello World!";
   char destination[20]; // make sure this array is big enough for original
   strcpy(destination, original);
-  printf("Copied string: %s\n", destination);
+  // printf("Copied string: %s\n", destination);
+
+  char greeting[20] = "Hello, ";
+  char name[] = "Justin";
+  strcat(greeting, name);
+  printf("Message: %s\n", greeting);
 }
